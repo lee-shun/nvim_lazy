6ce60175bf6b0f3033e628bb4f4249ee52040d90
ShawnLee <lee970802@163.com>
2023-02-21 10:55:36 -0500

    update the lazy

---
+++ lua/plugins/sonokai.lua
@@ -0,0 +1,9 @@
+return {
+    "sainnhe/sonokai",
+    config = function()
+        vim.g.sonokai_better_performance = 1
+        vim.g.sonokai_current_word = "grey background"
+        vim.g.sonokai_diagnostic_virtual_text = "colored"
+        vim.g.sonokai_spell_foreground = "colored"
+        vim.cmd("colorscheme sonokai")
+    end, }
