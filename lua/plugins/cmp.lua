ed6ec37269a4ed75d75230db643fa3b7e899e86b
ShawnLee <lee970802@163.com>
2023-02-27 10:03:57 -0500

    u

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,151 @@
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-cmdline" },
+		{ "tzachar/cmp-tabnine", build = "./install.sh" },
+		{ "saadparwaiz1/cmp_luasnip" },
+		{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+		{ "lukas-reineke/cmp-under-comparator" },
+		{ "rafamadriz/friendly-snippets" },
+	}, 
+	config = function()
+		local luasnip = require("luasnip")
+		local cmp = require("cmp")
+
+		local kind_icons = {
+			Text = "",
+			Method = "",
+			Function = "",
+			Constructor = "",
+			Field = "",
+			Variable = "",
+			Class = "ﴯ",
+			Interface = "",
+			Module = "",
+			Property = "ﰠ",
+			Unit = "",
+			Value = "",
+			Enum = "",
+			Keyword = "",
+			Snippet = "",
+			Color = "",
+			File = "",
+			Reference = "",
+			Folder = "",
+			EnumMember = "",
+			Constant = "",
+			Struct = "",
+			Event = "",
+			Operator = "",
+			TypeParameter = "",
+			TN = "💡",
+			Snip = "",
+		}
+
+		require("cmp").setup({
+			formatting = {
+				format = function(entry, vim_item)
+					-- Kind icons
+					vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+					-- Source
+					vim_item.menu = ({
+						nvim_lsp = "[LSP]",
+						buffer = "[Buffer]",
+						luasnip = "[Snip]",
+						nvim_lua = "[Lua]",
+						treesitter = "[Treesitter]",
+						path = "[Path]",
+						nvim_lsp_signature_help = "[Sig]",
+						cmp_tabnine = "[TN]",
+					})[entry.source.name]
+					return vim_item
+				end,
+			},
+		})
+
+		local has_words_before = function()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			mapping = {
+				["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+				["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+				["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+				["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+				["<C-e>"] = cmp.mapping({ i = cmp.mapping.close(), c = cmp.mapping.close() }),
+				["<CR>"] = cmp.mapping({
+					i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+				}),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+					"c",
+				}),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+					"c",
+				}),
+			},
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lsp_signature_help" },
+				{ name = "path" },
+				{ name = "cmp_tabnine" },
+				{ name = "buffer" },
+				{ name = "luasnip" },
+				{ name = "treesitter" },
+			},
+		})
+
+		-- Use buffer source for `/`
+		cmp.setup.cmdline("/", {
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		-- Use cmdline & path source for ':'
+		cmp.setup.cmdline(":", {
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+		})
+
+		-- Auto pairs
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+	end,
+}
