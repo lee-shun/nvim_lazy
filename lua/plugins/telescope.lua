ddcfaaaf94ee6e90903f6a980237c1738508ee80
ShawnLee <lee970802@163.com>
2023-02-27 09:37:59 -0500

    update the tele

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,89 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	cmd = { "Telescope" },
+	keys = { "<leader>f" },
+	dependencies = {
+		{ "nvim-lua/popup.nvim", opt = true },
+		{ "nvim-lua/plenary.nvim", opt = true },
+		{ "kyazdani42/nvim-web-devicons", opt = true },
+		{
+			"tibabit/vim-templates",
+			opt = true,
+			cmd = { "TemplateInit", "TemplateExpand" },
+			config = function()
+				local global = require("config.global")
+				vim.g.tmpl_auto_initialize = 0
+				vim.g.tmpl_search_paths = { global.vim_config_path .. "/template" }
+				vim.g.tmpl_author_name = "ShunLi"
+				vim.g.tmpl_author_email = "2015097272@qq.com"
+			end,
+		},
+	},
+	config = function()
+		local present, telescope = pcall(require, "telescope")
+		if not present then
+			return
+		end
+
+		local actions = require("telescope.actions")
+
+		telescope.setup({
+			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+				},
+				prompt_prefix = "   ",
+				selection_caret = " ",
+				mappings = {
+					i = {
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-n>"] = actions.cycle_history_next,
+						["<C-p>"] = actions.cycle_history_prev,
+					},
+				},
+				initial_mode = "insert",
+				selection_strategy = "reset",
+				sorting_strategy = "descending",
+				layout_strategy = "horizontal",
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+						preview_width = 0.55,
+						results_width = 0.8,
+					},
+					vertical = {
+						mirror = false,
+					},
+					width = 0.87,
+					height = 0.80,
+					preview_cutoff = 120,
+				},
+				file_sorter = require("telescope.sorters").get_fuzzy_file,
+				file_ignore_patterns = {},
+				generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+				path_display = { "truncate" },
+				winblend = 0,
+				border = {},
+				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				color_devicons = true,
+				use_less = true,
+				set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+				file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+				grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+				qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+				-- Developer configurations: Not meant for general override
+				buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+			},
+		})
+
+		-- extensions
+		require("telescope").load_extension("find_template")
+	end,
+}
---
+++ lua/telescope/_extensions/find_template.lua
@@ -0,0 +1,59 @@
+local telescope = require("telescope")
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local sorters = require("telescope.sorters")
+local conf = require("telescope.config").values
+
+-- the template dir
+local conf_dir = require("config.global").vim_config_path
+local plugin_dir = require("config.global").plugins_installed_path
+local tmpl_dir = { conf_dir .. "/template", plugin_dir .. "/site/pack/packer/opt/vim-templates/templates" }
+local tmpl_full_list = {}
+for _, d in pairs(tmpl_dir) do
+	local names = vim.fn.readdir(d)
+	for _, name in pairs(names) do
+		local item = vim.fn.fnamemodify(name, ":r")
+		local len = #vim.fn.split(d, "/")
+		local rel_path = vim.fn.split(d, "/")[len]
+		table.insert(tmpl_full_list, { item, rel_path .. "/" .. item, d .. "/" .. name })
+	end
+end
+
+-- prepare the finder
+local function apply_template(prompt_bufnr)
+	actions.close(prompt_bufnr)
+	local selection = action_state.get_selected_entry()
+	local cmd = "TemplateInit " .. selection.ordinal
+	vim.cmd(cmd)
+end
+
+local find_template = function(opts)
+	opts = opts or {}
+
+	pickers
+		.new(opts, {
+			prompt_title = "find in templates",
+			results_title = "templates",
+			finder = finders.new_table({
+				results = tmpl_full_list,
+				entry_maker = function(entry)
+					return {
+						value = entry[3],
+						display = entry[2],
+						ordinal = entry[1],
+					}
+				end,
+			}),
+			sorter = sorters.get_generic_fuzzy_sorter({}),
+			previewer = conf.file_previewer({}),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(apply_template)
+				return true
+			end,
+		})
+		:find()
+end
+
+return telescope.register_extension({ exports = { find_template = find_template } })
