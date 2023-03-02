01dedd48fadb92275a031de7aa9305fdc2f4411d
ShawnLee <lee970802@163.com>
2023-03-01 23:21:20 -0500

    u

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,8 +11,8 @@ return {
 			},
 		},
 		presets = {
-			bottom_search = true,
-			command_palette = true,
+			bottom_search = false,
+			command_palette = false,
 			long_message_to_split = true,
 			inc_rename = true,
 			lsp_doc_border = true,
