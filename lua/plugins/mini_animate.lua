0f340da019886a1de472693c072d64babc188adc
ShawnLee <lee970802@163.com>
2023-03-03 11:41:33 -0500

    update

---
+++ lua/plugins/mini_animate.lua
@@ -0,0 +1,7 @@
+return {
+	"echasnovski/mini.animate",
+	event = "VeryLazy",
+	config = function (_, opts)
+        require("mini.animate").setup(opts)
+	end
+}
---
+++ lua/plugins/mini_bufremove.lua
@@ -0,0 +1,22 @@
+return {
+	"echasnovski/mini.bufremove",
+	event = "VeryLazy",
+  -- stylua: ignore
+  keys = {
+    { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
+    { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+  },
+	config = function(_, opts)
+		require("mini.bufremove").setup(opts)
+		require("which-key").register({
+			b = { name = "Buffer" },
+		}, {
+			mode = "n",
+			prefix = "<leader>",
+			buffer = nil,
+			silent = true,
+			noremap = true,
+			nowait = false,
+		})
+	end,
+}
