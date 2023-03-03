a248d86aaaa0a186b0b6ca8641c846ff7fed705e
ShawnLee <lee970802@163.com>
2023-03-03 11:41:33 -0500

    update

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -24,7 +24,7 @@ return {
     { "<leader>nl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
     { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History" },
     { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All" },
-    { "<c-f>", function() if not require("noice.sp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
+    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
     { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
   },
 	config = function(_, opts)
