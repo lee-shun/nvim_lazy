ba773588e3404933ac488377f35598e7e262109f
ShawnLee <lee970802@163.com>
2023-03-05 15:42:48 -0500

    u

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,21 @@
-vim.opt_local.tabstop=2
-vim.opt_local.softtabstop=2
-vim.opt_local.shiftwidth=2
-vim.opt_local.spell=true
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.spell = true
+
+local buf = vim.api.nvim_get_current_buf()
+require("which-key").register({
+	["rm"] = {
+		function()
+			vim.cmd([[exec "MarkdownPreview" ]])
+		end,
+		"Preview",
+	},
+}, {
+	mode = "n",
+	prefix = "<leader>",
+	buffer = buf,
+	silent = true,
+	noremap = true,
+	nowait = false,
+})
