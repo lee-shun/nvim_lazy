6ff0b670b02eb73b728530eee62d40e451b56915
ShawnLee <lee970802@163.com>
2023-04-18 15:29:14 -0400

    update

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,3 +1,4 @@
+-- TODO: add m J K
 return {
 	"nvim-tree/nvim-tree.lua",
 	enabled = true,
@@ -7,6 +8,12 @@ return {
 	cmd = { "NvimTreeToggle", "NvimTreeClose" },
 	dependencies = { "kyazdani42/nvim-web-devicons" },
 	opts = {
+		ui = {
+			confirm = {
+				remove = true,
+				trash = false,
+			},
+		},
 		sync_root_with_cwd = true,
 		respect_buf_cwd = true,
 		update_focused_file = {
@@ -21,6 +28,32 @@ return {
 			local opts = function(desc)
 				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 			end
+
+			-- customized mappings
+			local mark_move_j = function()
+				api.marks.toggle()
+				vim.cmd("norm j")
+			end
+			local mark_move_k = function()
+				api.marks.toggle()
+				vim.cmd("norm k")
+			end
+			local bulk_trash = function()
+				local nodes = api.marks.list()
+				if next(nodes) == nil then
+					table.insert(nodes, api.get_node_under_cursor())
+				end
+				vim.ui.input({ prompt = string.format("Trash %s files? [y/n] ", #nodes) }, function(input)
+					if input == "y" then
+						for _, node in ipairs(nodes) do
+							api.fs.trash(node)
+						end
+                        api.marks.clear()
+						api.tree.reload()
+					end
+				end)
+			end
+
 			vim.keymap.set("n", "<CR>", api.tree.change_root_to_node, opts("CD"))
 			vim.keymap.set("n", "<C-e>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
 			vim.keymap.set("n", "e", api.node.open.horizontal, opts("Open: Horizontal Split"))
@@ -28,11 +61,10 @@ return {
 			vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
 			vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
 			vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
-			vim.keymap.set("n", "i", api.node.open.preview, opts("Open Preview"))
-			vim.keymap.set("n", ".", api.node.run.cmd, opts("Run Command"))
+			vim.keymap.set("n", "!", api.node.run.cmd, opts("Run Vim Command"))
 			vim.keymap.set("n", "<BS>", api.tree.change_root_to_parent, opts("Up"))
 			vim.keymap.set("n", "a", api.fs.create, opts("Create"))
-			vim.keymap.set("n", "i", api.node.show_info_popup, opts("Info"))
+			vim.keymap.set("n", "if", api.node.show_info_popup, opts("Info"))
 			vim.keymap.set("n", "dF", api.fs.remove, opts("Delete"))
 			vim.keymap.set("n", "df", api.fs.trash, opts("Trash"))
 			vim.keymap.set("n", "L", api.tree.expand_all, opts("Expand All"))
@@ -43,7 +75,8 @@ return {
 			vim.keymap.set("n", "<C-f>", api.live_filter.clear, opts("Clean Filter"))
 			vim.keymap.set("n", "f", api.live_filter.start, opts("Filter"))
 			vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-			vim.keymap.set("n", "m", api.marks.toggle, opts("Toggle Bookmark"))
+			vim.keymap.set("n", "J", mark_move_j, opts("Toggle Bookmark Down"))
+			vim.keymap.set("n", "K", mark_move_k, opts("Toggle Bookmark Up"))
 			vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
 			vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
 			vim.keymap.set("n", "q", api.tree.close, opts("Close"))
@@ -61,30 +94,14 @@ return {
 			vim.keymap.set("n", "yP", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
 
 			-- special
-  			vim.keymap.set("n", "md", api.marks.bulk.move, opts("Move Bookmarked"))
+			vim.keymap.set("n", "md", api.marks.bulk.move, opts("Move Bookmarked"))
+			vim.keymap.set("n", "mt", bulk_trash, opts("Bulk Trash Files"))
 			vim.keymap.set("n", "[g", api.node.navigate.git.prev, opts("Prev Git"))
 			vim.keymap.set("n", "]g", api.node.navigate.git.next, opts("Next Git"))
 			vim.keymap.set("n", ">", api.node.navigate.sibling.next, opts("Next Sibling"))
 			vim.keymap.set("n", "<", api.node.navigate.sibling.prev, opts("Previous Sibling"))
 			vim.keymap.set("n", "gg", api.node.navigate.sibling.last, opts("Last Sibling"))
 			vim.keymap.set("n", "G", api.node.navigate.sibling.first, opts("First Sibling"))
-
-			local bulk_trash = function()
-				local nodes = api.marks.list()
-				if next(nodes) == nil then
-					table.insert(nodes, api.get_node_under_cursor())
-				end
-				vim.ui.input({ prompt = string.format("Trash %s files? [y/n] ", #nodes) }, function(input)
-					if input == "y" then
-						for _, node in ipairs(nodes) do
-							vim.fn.jobstart("trash-put " .. node.absolute_path)
-						end
-						api.tree.reload()
-					end
-				end)
-			end
-
-			vim.keymap.set("n", "mt", bulk_trash, opts("Bulk Trash Files"))
 		end,
 	},
 }
