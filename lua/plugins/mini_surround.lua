0705556c14de1c6e5cf639afd67f7e1242f41583
ShawnLee <lee970802@163.com>
2023-04-18 10:31:22 -0400

    update

--- lua/plugins/mini_surround.lua
+++ lua/plugins/mini_surround.lua
@@ -20,13 +20,13 @@ return {
 	end,
 	opts = {
 		mappings = {
-			add = "gza", -- Add surrounding in Normal and Visual modes
-			delete = "gzd", -- Delete surrounding
-			find = "gzf", -- Find surrounding (to the right)
-			find_left = "gzF", -- Find surrounding (to the left)
-			highlight = "gzh", -- Highlight surrounding
-			replace = "gzr", -- Replace surrounding
-			update_n_lines = "gzn", -- Update `n_lines`
+			add = "sa", -- Add surrounding in Normal and Visual modes
+			delete = "sd", -- Delete surrounding
+			find = "sf", -- Find surrounding (to the right)
+			find_left = "sF", -- Find surrounding (to the left)
+			highlight = "sh", -- Highlight surrounding
+			replace = "sr", -- Replace surrounding
+			update_n_lines = "sn", -- Update `n_lines`
 
 			suffix_last = "l", -- Suffix to search with "prev" method
 			suffix_next = "n", -- Suffix to search with "next" method
