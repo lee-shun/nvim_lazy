515eb2ab676913002590b6af0e308ef2b7819dc8
Shun Li <shun.li.at.casia@outlook.com>
2023-07-31 09:07:45 +0800

    up

--- lua/plugins/auto_pairs.lua
+++ lua/plugins/auto_pairs.lua
@@ -1,10 +1,11 @@
 return {
 	"windwp/nvim-autopairs",
-	event = "VeryLazy",
+    event = "InsertEnter",
 	config = function()
 		local npairs = require("nvim-autopairs")
 		npairs.setup({
 			check_ts = true,
+            disable_filetype = { "TelescopePrompt"},
 		})
 		npairs.add_rules(require("nvim-autopairs.rules.endwise-lua"))
 	end,
