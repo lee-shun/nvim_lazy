eff2ea4c596db2b99ddc9f73e847ea35c8ccd701
shun li <is.shun.li@outlook.com>
2025-03-28 01:22:03 +0800

    update vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,7 @@ return {
     ft = "tex",
     config = function()
         vim.g.vimtex_view_method = "zathura"
-        vim.g.vimtex_quickfix_mode = 0
+        vim.g.vimtex_quickfix_mode = 1
         vim.g.vimtex_mappings_enabled = 0
         vim.g.vimtex_imaps_enabled = 0
         vim.g.vimtex_text_obj_enabled = 0
@@ -13,20 +13,5 @@ return {
         vim.g.vimtex_format_enabled = 0
         vim.g.vimtex_syntax_enabled = 0
         vim.g.vimtex_compiler_silent = 1
-        vim.cmd([[
-            let g:vimtex_compiler_latexmk = {
-        \ 'build_dir' : '',
-        \ 'callback' : 1,
-        \ 'continuous' : 1,
-        \ 'executable' : 'latexmk',
-        \ 'hooks' : [],
-        \ 'options' : [
-        \   '-verbose',
-        \   '-file-line-error',
-        \   '-synctex=1',
-        \   '-interaction=nonstopmode',
-        \ ],
-        \}
-        ]])
     end,
 }
